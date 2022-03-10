56ef9454e9eddb3e66106acf2d686ddddad1d4b5
zenjutahi <zenjutahi@gmail.com>
2022-03-10 12:23:47 +0300

    spring demo project

---
+++ src/main/java/com/javasetup/trial/javasetupdemo/JavasetupDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javasetup.trial.javasetupdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasetupDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasetupDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javasetup/trial/javasetupdemo/JavasetupDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasetup.trial.javasetupdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasetupDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
